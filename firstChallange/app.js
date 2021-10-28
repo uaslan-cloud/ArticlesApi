832c19d669189569efd346e5b0f4f0a04f0a9f71
UmutAslan-cloud <umutchinax@gmail.com>
2021-10-28 11:29:14 +0200

    We created a simple Articles API and fetch it

---
+++ firstChallange/app.js
@@ -0,0 +1,19 @@
+let main=document.getElementById("main-content")
+let input=document.getElementById("search-input")
+let button=document.getElementById("enter")
+
+button.addEventListener("click",function () {
+let valueofInput=input.value
+console.log(valueofInput)
+    fetch(`https://newsapi.org/v2/everything?q=${valueofInput}&apiKey=67894c4e782d40c0b271c7c27a97aca4`)
+    .then(response => response.json())
+    .then(data => {console.log(data)
+        let list=data.articles
+        let result=list.map(function (listing) {
+            return `<p>${listing.title}</p>
+            <p>${listing.author}</p>
+            <p>${listing.description}</p>`
+        }).join("");
+    main.innerHTML=result
+    })
+})
